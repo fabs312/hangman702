cdac22b4ea9f9a70b247830cd99514de8e90af88
fabs312 <fabionascimento@hotmail.co.uk>
2023-11-12 19:24:22 +0000

    conditonal to accept at least one letter

--- milestones_2.py
+++ milestones_2.py
@@ -2,9 +2,19 @@ import random
 
 # create list of fave list
 fave_fruits = ["watermelon", "banana", "kiwi", "guava", "apple"]
-# use random module to pick a fruit
-word = random.choice(fave_fruits)
 
+# use random module to pick a fruit
+word_list= fave_fruits
+word = random.choice(word_list)
+
+# create guess input function
 guess = input("Enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good Guess!")
+else:
+    print("Ooops! That is not a valid input")
+
 print(word)
+
 
