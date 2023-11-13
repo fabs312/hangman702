b1a607c89cce9b9f497d03878db8040b0a6ff84c
fabs312 <fabionascimento@hotmail.co.uk>
2023-11-13 23:48:27 +0000

    implementation if letter not in word

--- milestone_4.1.py
+++ milestone_4.1.py
@@ -11,38 +11,40 @@ class Hangman:
         print(f"The mystery word has {self.num_letters} characters")
         print(self.word_guessed)
 
-def check_guess(self, guess):
-    letter = letter.lower()
-    if guess in self.word:
-        print(f"Good guess! {guess} is in the word.")
-        for i in range(len(self.word)):
-            if self.word[i] == guess:
-                self.word_guessed[i] = guess
-        self.num_letters -= 1
-    else:
-        self.num_lives -= 1
-        print(f"Sorry, {letter} is not in the word. Try again")
-    
-def ask_for_input(self):
-    while True:
-        guess = input("Enter one letter: ").lower()
-        if len(guess) !=1:
-            print("Invalid letter. Please, enter one single letter.")
-        elif guess in self.list_of_letters:
-            print("You already tried that letter!")
+    def check_letter(self, letter):
+        letter = letter.lower()
+        if letter in self.word:
+            print(f"Good guess! {letter} is in the word.")
+            for i in range(len(self.word)):
+                if self.word[i] == letter:
+                    self.word_guessed[i] = letter
+            self.num_letters -= 1
         else:
-            self.list_letters.append(guess)
-            self.check_guess(guess)
-            break
+            self.num_lives -= 1
+            print(f"Sorry, {letter} is not in the word. Try again")
+    
+    def ask_letter(self):
+        while True:
+            letter = input("Enter one letter: ").lower()
+            if len(letter) != 1:
+                print("Invalid letter. Please, enter one single letter.")
+            elif letter in self.list_letters:
+                print("You already tried that letter!")
+            else:
+                self.list_letters.append(letter)
+                self.check_letter(letter)
+                break
  
 def play_game(word_list):
     game = Hangman(word_list, num_lives=5)
     while game.num_lives > 0 and game.num_letters > 0:
-        game.ask_for_input()
+        game.ask_letter()
     if game.num_letters == 0:
         print("You won")
     else:
         print(f"You lost. The word was {game.word}")
+    
+    
 
  # intialise game
 if __name__ == "__main__":
